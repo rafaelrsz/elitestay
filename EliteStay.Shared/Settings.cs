8ec41af84455d6b96249f75f19beeec883225d79
rafaelrsz <rafa.rsouz@gmail.com>
2023-07-13 16:49:56 -0300

    build: added database settings file

---
+++ EliteStay.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace EliteStay.Shared
+{
+  public static class Settings
+  {
+    public static string ConnectionString = @"Server=DESKTOP-01BBK2K;Database=elitestay;User ID=sa;Password=sa;";
+  }
+}
