9afa1126ad72a88131713d4671ae07e1ac2c5231
rafaelrsz <rafa.rsouz@gmail.com>
2023-07-15 00:53:01 -0300

    test: added entities tests

--- EliteStay.Tests/Entities/RoomTests.cs
+++ EliteStay.Tests/Entities/RoomTests.cs
@@ -1,8 +1,16 @@
+using EliteStay.Domain.BookingContext.Entities;
+
 namespace EliteStay.Tests.Entities
 {
   [TestClass]
   public class RoomTests
   {
-
+    [TestMethod]
+    public void ShouldBeAbleToCreateARoom()
+    {
+      var room = new Room("Example Room", "Ap404", 50, 2);
+      Assert.AreEqual(true, room.Valid);
+      Assert.AreEqual(0, room.Notifications.Count);
+    }
   }
 }
--- EliteStay.Tests/Entities/UserTests.cs
+++ EliteStay.Tests/Entities/UserTests.cs
@@ -1,8 +1,34 @@
+using EliteStay.Domain.BookingContext.Entities;
+using EliteStay.Domain.BookingContext.Enums;
+using EliteStay.Domain.BookingContext.ValueObjects;
+
 namespace EliteStay.Tests.Entities
 {
   [TestClass]
   public class UserTests
   {
-    // nao deve ser possivel criar um usuario menor de 18 anos
+    [TestMethod]
+    public void ShouldBeAbleToCreateAnUser()
+    {
+      var name = new Name("FirstName", "LastName");
+      var email = new Email("example@email.com");
+      var document = new Document("40438088018");
+      var user = new User(name, "", email, document, "37999584571", 18, EUserPermission.Normal);
+
+      Assert.AreEqual(true, user.Valid);
+      Assert.AreEqual(0, user.Notifications.Count);
+    }
+
+    [TestMethod]
+    public void ShouldNotBeAbleToCreateAnUserWithInsuficientAge()
+    {
+      var name = new Name("FirstName", "LastName");
+      var email = new Email("example@email.com");
+      var document = new Document("40438088018");
+      var user = new User(name, "", email, document, "37999584571", 15, EUserPermission.Normal);
+
+      Assert.AreEqual(false, user.Valid);
+      Assert.AreEqual(1, user.Notifications.Count);
+    }
   }
 }
